d99b016ef3b7ec1043de38c491b6f1d6a09cbb91
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-07-22 20:06:38 +0530

    Disable ESLint on Build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true, // helpful for development
+  reactStrictMode: true, 
   experimental: {
-    serverActions: true,
+    serverActions: {}, 
   },
 };
 
